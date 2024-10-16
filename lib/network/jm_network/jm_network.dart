cd239f3f4c206d7170a48f95bee4d7ee00497a3b
onlytheworld <37205103+onlytheworld@users.noreply.github.com>
2024-10-16 15:30:21 +0800

    Update jm_network.dart

--- lib/network/jm_network/jm_network.dart
+++ lib/network/jm_network/jm_network.dart
@@ -84,8 +84,8 @@ class JmNetwork {
   static const urls = <String>[
     "https://www.jmeadpoolcdn.one",
     "https://www.jmeadpoolcdn.life",
-    "https://www.jmapiproxyxxx.one",
-    "https://www.jmfreedomproxy.xyz",
+    // "https://www.jmapiproxyxxx.one",
+    // "https://www.jmfreedomproxy.xyz",
     "https://18comic.vip",
     "https://18comic-16promax.club"
   ];
